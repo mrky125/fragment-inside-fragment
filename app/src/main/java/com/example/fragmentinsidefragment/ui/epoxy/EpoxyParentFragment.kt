04b114072e5f4f0b179a56aee3b368d728a4933d
mrky125 <4ex21ia@gmail.com>
2021-05-13 12:58:57 +0900

    observe list of viewModel

--- app/src/main/java/com/example/fragmentinsidefragment/ui/epoxy/EpoxyParentFragment.kt
+++ app/src/main/java/com/example/fragmentinsidefragment/ui/epoxy/EpoxyParentFragment.kt
@@ -5,27 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.fragmentinsidefragment.databinding.FragmentEpoxyParentBinding
+import com.example.fragmentinsidefragment.viewmodel.EpoxyViewModel
 
 class EpoxyParentFragment : Fragment() {
 
     private lateinit var binding: FragmentEpoxyParentBinding
+    private lateinit var viewModel: EpoxyViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setupViewModel()
         return FragmentEpoxyParentBinding.inflate(inflater).let {
             binding = it
+            binding.viewModel = viewModel
             it.root
         }
     }
 
+    private fun setupViewModel() {
+        viewModel = ViewModelProvider.NewInstanceFactory().create(EpoxyViewModel::class.java)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
         setupEpoxyAdapter()
+        setViewModelList()
+    }
+
+    private fun setViewModelList() {
+        viewModel.apply {
+            setFirstList()
+            setSecondList()
+        }
     }
 
     private fun setupEpoxyAdapter() {
@@ -39,10 +57,10 @@ class EpoxyParentFragment : Fragment() {
                     spanSizeLookup = controller.spanSizeLookup
                 }
         }
-        controller.setData(
-            listOf("one", "two", "three", "four", "five", "six", "seven", "eight"),
-            listOf("foo", "bar")
-        )
+        controller.setData(emptyList(), listOf("foo", "bar"))
+        viewModel.firstList.observe(viewLifecycleOwner, {
+            controller.setData(it, listOf("foo", "bar"))
+        })
     }
 
 }
--- app/src/main/java/com/example/fragmentinsidefragment/viewmodel/EpoxyViewModel.kt
+++ app/src/main/java/com/example/fragmentinsidefragment/viewmodel/EpoxyViewModel.kt
@@ -1,4 +1,28 @@
 package com.example.fragmentinsidefragment.viewmodel
 
-class EpoxyViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class EpoxyViewModel : ViewModel() {
+
+    val firstList: MutableLiveData<List<String>> = MutableLiveData(emptyList())
+    val secondList: MutableLiveData<List<String>> = MutableLiveData(emptyList())
+
+    fun setFirstList() {
+        viewModelScope.launch {
+            delay(1000)
+            firstList.value = listOf("one", "two", "three", "four", "five", "six", "seven", "eight")
+        }
+    }
+
+    fun setSecondList() {
+        viewModelScope.launch {
+            delay(1000)
+            secondList.value = listOf("one", "two", "three", "four", "five", "six")
+        }
+    }
+
 }
