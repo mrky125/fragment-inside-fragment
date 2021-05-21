63c837a063d6f4992ff27ffdd03809b49c37436f
mrky125 <4ex21ia@gmail.com>
2021-05-21 10:20:21 +0900

    diplay different view type by position

--- app/src/main/java/com/example/fragmentinsidefragment/ui/epoxy/controller/EpoxyPagingController.kt
+++ app/src/main/java/com/example/fragmentinsidefragment/ui/epoxy/controller/EpoxyPagingController.kt
@@ -3,15 +3,26 @@ package com.example.fragmentinsidefragment.ui.epoxy.controller
 import androidx.lifecycle.MutableLiveData
 import com.airbnb.epoxy.EpoxyModel
 import com.airbnb.epoxy.paging.PagedListEpoxyController
+import com.example.fragmentinsidefragment.ui.epoxy.model.ItemFooterModel_
 import com.example.fragmentinsidefragment.ui.epoxy.model.ItemHeaderModel_
 
 class EpoxyPagingController : PagedListEpoxyController<String>() {
 
     override fun buildItemModel(currentPosition: Int, item: String?): EpoxyModel<*> {
-        return ItemHeaderModel_().apply {
-            id(currentPosition)
-            item?.also {
-                item(MutableLiveData(it))
+        return when (currentPosition) {
+            3, 4, 10, 11, 24, 30, 31, 40, 41, 42, 63, 67, 77, 89, 93 -> {
+                ItemFooterModel_().apply {
+                    id(currentPosition)
+                    footer(item)
+                }
+            }
+            else -> {
+                ItemHeaderModel_().apply {
+                    id(currentPosition)
+                    item?.also {
+                        item(MutableLiveData(it))
+                    }
+                }
             }
         }
     }
